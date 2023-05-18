e2a25eb02ee54aa4bb05bf8edbe8986026405003
Emroze <emroze.petaflop@gmail.com>
2023-05-18 11:34:36 +0600

    Google Authentication Added

--- ecommerce-admin/app/page.js
+++ ecommerce-admin/app/page.js
@@ -1,5 +1,37 @@
+"use client"
+import { useSession, signIn, signOut } from "next-auth/react"
+
 export default function Home() {
+  const { data:session } = useSession();
+  if(!session){
+    return (
+      <div className="bg-blue-900 w-screen h-screen flex items-center">
+        <div className="text-center w-full">
+          <button onClick={() => signIn('google')} className="bg-white p-2 px-4 rounded-lg">Login with google</button>
+        </div>
+      </div>
+    )
+  }
   return (
-    <div>Hello</div>
+    <div>
+      <div>Logged in {session.user.email}</div>
+    </div>
   )
+  
 }
+
+
+
+// export default function Component() {
+//   const { data: session } = useSession()
+//   if(session) {
+//     return <>
+//       Signed in as {session.user.email} <br/>
+//       <button onClick={() => signOut()}>Sign out</button>
+//     </>
+//   }
+//   return <>
+//     Not signed in <br/>
+//     <button onClick={() => signIn()}>Sign in</button>
+//   </>
+// }
